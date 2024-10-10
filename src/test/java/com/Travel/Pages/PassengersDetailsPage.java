0a0772d0bf3e0672f9ef10f7dc335adad5671108
Muhammad Qaiser <muhammadqaiser64@yahoo.com>
2024-10-10 07:32:59 +0100

    more new steps added

--- src/test/java/com/Travel/Pages/PassengersDetailsPage.java
+++ src/test/java/com/Travel/Pages/PassengersDetailsPage.java
@@ -12,16 +12,19 @@ public class PassengersDetailsPage
 	{
 		this.driver=driver;
 	}
-	
+	protected By passengeer_details_page_con = By.xpath("//h2[normalize-space()='Passenger Details']"); 
 	protected By first_name = By.xpath("//input[@name='passengerFirstName']");
 	protected By last_name = By.xpath("//input[@name='passengerLastName']");
 	protected By next_btn = By.xpath("//input[@value='Next']");
 	
-	public void enterPassengerDetails(String name, String l_name)
+	public PaymentDetailsPage enterPassengerDetails(String name, String l_name)
 	{
+		Utility.waitForWebElement(driver, passengeer_details_page_con).isDisplayed();
 		Utility.waitForWebElement(driver, first_name).sendKeys(name);
 		Utility.waitForWebElement(driver, last_name).sendKeys(l_name);
 		Utility.waitForWebElement(driver, next_btn).click();
+		PaymentDetailsPage pdp=new PaymentDetailsPage(driver);
+		return pdp;
 		
 	}
 
